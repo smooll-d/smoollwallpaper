3b07d2ae689f67d31ae3d16c4f389f92eac45d4b
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-15 20:05:27 +0200

    swall_image.py: Refactoring and terminal output formatting

--- src/swall_image.py
+++ src/swall_image.py
@@ -1,20 +1,20 @@
+#!/usr/bin/python3
+
 import sys
 
 from colorama import init, deinit, Fore, Style
 
 import utils
 from config_file import config_file
-from convert_all_to_bmp import convert_all_to_bmp
 from start_img_wallpaper import start_img_wallpaper
 
 def main():
     init()
 
     config_file()
-    convert_all_to_bmp()
     try:
         print("\nswall: Path = " + config_file.path_img)
-        print("swall: Converted image path = " + convert_all_to_bmp.converted_image_path)
+        print("swall: Colorscheme = " + config_file.colorscheme)
     except AttributeError:
         print("\nPlease set your path in the swall.conf config file.")
         print(f"You can find it at \"{utils.CONFIG_FILE}\".")
