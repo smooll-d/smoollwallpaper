e6338c04cd17c0364d39080cedf7c400898b5a79
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-15 20:03:01 +0200

    Removed `convert_all_to_bmp.py`

--- src/convert_all_to_bmp.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-from config_file import config_file
-
-def convert_all_to_bmp():
-    convert_all_to_bmp.img = Image.open(config_file.path_img)
-    convert_all_to_bmp.img.save(f"{config_file.path_img}.bmp")
-
-    convert_all_to_bmp.converted_image_path = f"{config_file.path_img}.bmp"
