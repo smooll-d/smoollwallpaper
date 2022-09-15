5e711003b21f4beed0617cc850c16bbc38b28de5
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-15 20:04:36 +0200

    start_img_wallpaper.py: Added `GNOME` wallpaper changing

--- src/start_img_wallpaper.py
+++ src/start_img_wallpaper.py
@@ -1,6 +1,11 @@
 import os
 
-from convert_all_to_bmp import convert_all_to_bmp
+import utils
+from config_file import config_file
 
 def start_img_wallpaper():
-    os.system(f"xsetroot -bitmap {convert_all_to_bmp.converted_image_path}")
+    if utils.XDG_CURRENT_DESKTOP == "GNOME":
+        if config_file.colorscheme == "light":
+            os.system(f"gsettings set org.gnome.desktop.background picture-uri \"file://{config_file.path_img}\"")
+        elif config_file.colorscheme == "dark":
+            os.system(f"gsettings set org.gnome.desktop.background picture-uri-dark \"file://{config_file.path_img}\"")
