f0dc2bb2c238b14de412e7499496b09397e7fca6
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-16 19:33:46 +0200

    start_img_wallpaper.py: Added `KDE` wallpaper changing and simplified GNOME wallpaper changing

--- src/start_img_wallpaper.py
+++ src/start_img_wallpaper.py
@@ -1,11 +1,27 @@
 import os
+import dbus
 
 import utils
 from config_file import config_file
 
+def setKDEwallpaper(filepath, plugin="org.kde.image"):
+    jscript = """
+var allDesktops = desktops();
+for (i = 0; i < allDesktops.length; i++) {
+    d = allDesktops[i];
+    d.wallpaperPlugin = "%s";
+    d.currentConfigGroup = Array("Wallpaper", "%s", "General");
+    d.writeConfig("Image", "file://%s")
+}
+"""
+    bus = dbus.SessionBus();
+    plasma = dbus.Interface(bus.get_object(
+        "org.kde.plasmashell", "/PlasmaShell"), dbus_interface="org.kde.PlasmaShell")
+    plasma.evaluateScript(jscript % (plugin, plugin, filepath))
+
 def start_img_wallpaper():
     if utils.XDG_CURRENT_DESKTOP == "GNOME":
-        if config_file.colorscheme == "light":
-            os.system(f"gsettings set org.gnome.desktop.background picture-uri \"file://{config_file.path_img}\"")
-        elif config_file.colorscheme == "dark":
-            os.system(f"gsettings set org.gnome.desktop.background picture-uri-dark \"file://{config_file.path_img}\"")
+        os.system(f"gsettings set org.gnome.desktop.background picture-uri \"file://{config_file.path_img}\"")
+        os.system(f"gsettings set org.gnome.desktop.background picture-uri-dark \"file://{config_file.path_img}\"")
+    elif utils.KDE_FULL_SESSION == True:
+        setKDEwallpaper(config_file.path_img)
