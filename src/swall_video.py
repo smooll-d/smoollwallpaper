0b961c658a0dcdcc96057c5bde600bb368b87401
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-23 21:03:57 +0200

    swall_video.py: Change logging system

--- src/swall_video.py
+++ src/swall_video.py
@@ -9,35 +9,37 @@ import utils
 from config_file import config_file
 from pause_wallpaper_on_focus_lost import pause_wallpaper_on_focus_lost
 from start_wallpaper import start_wallpaper
+from SwallLogger import SwallLogger
+
+sl = SwallLogger("swall")
 
 def main():
     init()
 
     config_file()
     try:
-        print("\nswall: Path = " + config_file.path_vid)
-        print("swall: Opacity = " + config_file.opacity)
-        print("swall: Priority level = " + config_file.priority)
-        print("swall: Pause = " + config_file.pause)
-        print("\nswall: Desktop WID = " + config_file.dwid)
+        sl.info("Path = " + config_file.path_vid)
+        sl.info("Opacity = " + config_file.opacity)
+        sl.info("Priority level = " + config_file.priority)
+        sl.info("Pause = " + config_file.pause + "\n")
+        sl.info("Desktop WID = " + config_file.dwid + "\n")
     except AttributeError:
-        print("\nPlease set your path in the swall.conf config file.")
-        print(f"You can find it at \"{utils.CONFIG_FILE}\".")
+        sl.warning("Please set your path in the swall.conf config file.")
+        sl.warning(f"You can find it at \"{utils.CONFIG_FILE}\".")
         sys.exit(0)
 
-    print("\nswall: Creating threads...")
+    sl.info("Creating threads...")
     thread_1 = threading.Thread(target=start_wallpaper)
     thread_2 = threading.Thread(target=pause_wallpaper_on_focus_lost)
 
-    print("swall: Starting threads...")
+    sl.info("Starting threads...\n")
     if config_file.pause == "true":
         thread_1.start()
         thread_2.start()
     elif config_file.pause == "false":
         thread_1.start()
 
-    print(Fore.GREEN + "\nswall: Running!")
-    print(Style.RESET_ALL)
+    sl.info(Fore.GREEN + "Running!" + Style.RESET_ALL + "\n")
 
     deinit()
 
