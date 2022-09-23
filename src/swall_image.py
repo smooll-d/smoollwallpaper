d6bb4c89fb2c94535e43be7d33df08e439935d60
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-23 21:03:41 +0200

    swall_image.py: Change logging system

--- src/swall_image.py
+++ src/swall_image.py
@@ -7,22 +7,24 @@ from colorama import init, deinit, Fore, Style
 import utils
 from config_file import config_file
 from start_img_wallpaper import start_img_wallpaper
+from SwallLogger import SwallLogger
+
+sl = SwallLogger("swall")
 
 def main():
     init()
 
     config_file()
     try:
-        print("\nswall: Path = " + config_file.path_img)
+        sl.info("Path = " + config_file.path_img + "\n")
     except AttributeError:
-        print("\nPlease set your path in the swall.conf config file.")
-        print(f"You can find it at \"{utils.CONFIG_FILE}\".")
+        sl.warning("Please set your path in the swall.conf config file.")
+        sl.warning(f"You can find it at \"{utils.CONFIG_FILE}\".")
         sys.exit(0)
 
     start_img_wallpaper()
 
-    print(Fore.GREEN + "\nswall: Set wallpaper!")
-    print(Style.RESET_ALL)
+    sl.info(Fore.GREEN + "Set wallpaper!" + Style.RESET_ALL)
 
     deinit()
 
