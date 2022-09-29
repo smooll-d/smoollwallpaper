e88c8e38d1e3ff94a53cec5c1059d49e5f33674d
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-29 19:27:46 +0200

    Rename `swall_image.py` to `SwallImage.py`

---
+++ src/SwallImage.py
@@ -0,0 +1,31 @@
+import sys
+
+from colorama import init, deinit, Fore, Style
+
+from imports import utils
+from imports.config_file import config_file
+from imports.start_img_wallpaper import start_img_wallpaper
+from SwallLogger import SwallLogger
+
+sl = SwallLogger("swall")
+
+class SwallImage:
+    def __init__(self):
+        pass
+
+    def swall_image(self):
+        init()
+    
+        config_file()
+        try:
+            sl.info("Path = " + config_file.path_img + "\n")
+        except AttributeError:
+            sl.warning("Please set your path in the swall.conf config file.")
+            sl.warning(f"You can find it at \"{utils.CONFIG_FILE}\".")
+            sys.exit(0)
+
+        start_img_wallpaper()
+
+        sl.info(Fore.GREEN + "Set wallpaper!" + Style.RESET_ALL)
+
+        deinit()
