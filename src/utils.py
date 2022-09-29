41fcecea72fa78fe031a69a7b80a9a1c450ad62d
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-29 19:26:00 +0200

    Move to `imports/` directory

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-HOME                = os.getenv("HOME")
-XDG_CURRENT_DESKTOP = os.getenv("XDG_CURRENT_DESKTOP")
-KDE_FULL_SESSION    = os.getenv("KDE_FULL_SESSION") # Only if user's inside KDE
-
-CONFIG_FOLDER = f"{HOME}/.config/swall/"
-CONFIG_FILE   = f"{HOME}/.config/swall/swall.conf"
