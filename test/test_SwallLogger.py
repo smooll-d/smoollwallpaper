e2a417461f4c28feee432d4555b4ceaefdddd7b5
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-30 18:12:20 +0200

    Made tests work properly :)

--- test/test_SwallLogger.py
+++ test/test_SwallLogger.py
@@ -1,13 +1,19 @@
-def test_SwallLogger():
+import pytest
+
+@pytest.mark.parametrize("message", [
+        "This is a debug message.",
+        "This is an info message.",
+        "This is a warning message.",
+        "This is an error message.",
+        "This is a critical message."
+    ])
+def test_SwallLogger(message):
     from src.SwallLogger import SwallLogger
 
     sl = SwallLogger("test_Swalllogger")
-
-    sl.debug("This is a debug message.")
-    sl.info("This is an info message.")
-    sl.warning("This is a warning message.")
-    sl.error("This is an error message.")
-    sl.critical("This is a critical message.")
-
-if __name__ == "__main__":
-    test_SwallLogger()
+ 
+    sl.debug(message[0])
+    sl.info(message[1])
+    sl.warning(message[2])
+    sl.error(message[3])
+    sl.critical(message[4])
