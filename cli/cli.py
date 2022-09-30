cbe3fe1a1f62e2327b1bab32f4b12b32fff679fe
Jakub Skowron <jakubskowron676@gmail.com>
2022-09-30 20:23:32 +0200

    Got the cli to finally work

--- cli/cli.py
+++ cli/cli.py
@@ -1,15 +1,21 @@
 import click
 
-from ..src.SwallVideo import SwallVideo
-from ..src.SwallImage import SwallImage
+from src.SwallVideo import SwallVideo
+from src.SwallImage import SwallImage
+from src.__version__ import __version__
 
 sv = SwallVideo()
 si = SwallImage()
 
+def version():
+        print(f"SmoollWallpaper v.{__version__} made by Jakub Skowron <jakubskowron676@gmail.com>")
+
 @click.command()
+@click.option("--version", is_flag=True, callback=version, is_eager=True, expose_value=False, help="Show version.")
 def swall_vid():
     sv.swall_video()
 
 @click.command()
+@click.option("--version", is_flag=True, callback=version, is_eager=True, expose_value=False, help="Show version.")
 def swall_img():
     si.swall_image()
