82a5c4be55f381632ee0f07d07dd9337d8e2200a
Jakub Skowron <jakubskowron676@gmail.com>
2022-10-04 20:48:04 +0200

    Added `path` option to `vid` command

--- cli/cli.py
+++ cli/cli.py
@@ -3,27 +3,30 @@ import click
 from src.SwallVideo import SwallVideo
 from src.SwallImage import SwallImage
 from src import metadata
+from src.config_file import SwallConfig
 
 sv = SwallVideo()
 si = SwallImage()
+sc = SwallConfig()
 
 def swall_version():
     click.echo(f"{metadata.__name__} v.{metadata.__version__}")
 
 @click.group(invoke_without_command=True)
-@click.option("-v", "--version", is_flag=True, required=False, help="Show version.")
+@click.option("--version", is_flag=True, required=False, help="Show version.")
 def swall(version):
-    """_summary_
+    """Simple Python script for setting video wallpapers on Linux.
     """
     if version:
         swall_version()
         exit(0)
 
 @swall.command()
-def vid():
+@click.option("-p", "--path", required=False, show_default=True, default=sc.path_vid, help="Set path to wallpaper.")
+def vid(path):
     """Set video (.mp4) as wallpaper.
     """
-    sv.swall_video()
+    sv.swall_video(path)
 
 @swall.command()
 def img():
