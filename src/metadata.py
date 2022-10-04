7c8d8efc52c481c0865ba84a2a6815287ae90865
Jakub Skowron <jakubskowron676@gmail.com>
2022-10-04 20:49:24 +0200

    Version changes

--- src/metadata.py
+++ src/metadata.py
@@ -1,4 +1,4 @@
 __name__ 		= "SmoollWallpaper"
-__version__ 	= "1.4.0-alpha.65"
+__version__ 	= "1.4.0-testing.20"
 __author__  	= "Jakub Skowron (reallySmooll)"
 __email__		= "jakubskowron676@gmail.com"
